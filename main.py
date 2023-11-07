c10ccb93d5e927efcdd89ea4194fb6662e498514
alefrp <alefrodrigopereira@yahoo.com.br>
2023-11-07 15:45:11 -0300

    feat: Analise de Ações

--- main.py
+++ main.py
@@ -1,38 +1,89 @@
 import yfinance as yf
+import logging
 import modules.markovitz as mk
 import modules.multiplos as mp
 import modules.indicadores as ind
 
-class AnalisadorDeAcoes:
-    def __init__(self, tickers):
+# Configuração do logging
+logging.basicConfig(level=logging.INFO)
+
+class Acoes:
+    def __init__(self, tickers, periodo='1y'):
         self.tickers = tickers
+        self.periodo = periodo
         self.dados = None
 
     def baixar_dados(self):
-        self.dados = yf.download(self.tickers, period='1y')
+        try:
+            self.dados = yf.download(self.tickers, period=self.periodo)
+        except Exception as e:
+            logging.error(f"Erro ao baixar dados: {e}")
+            raise
+
+    def calcular_multiplos(self):
+        multiplos_resultados = {}
+        for ticker in self.tickers:
+            try:
+                dados_financeiros = yf.Ticker(ticker).info
+                eps = dados_financeiros.get('trailingEps') or dados_financeiros.get('forwardEps')
+                bvps = dados_financeiros.get('bookValue')
+                preco = self.dados['Adj Close'][ticker].iloc[-1]  # Preço mais recente
+                
+                if eps and bvps:
+                    pe = mp.calcular_pe(preco, eps)
+                    pvp = mp.calcular_pvp(preco, bvps)
+                else:
+                    pe = pvp = None
+
+                multiplos_resultados[ticker] = {
+                    'P/L': pe,
+                    'P/VP': pvp
+                }
+            except Exception as e:
+                logging.error(f"Erro ao calcular múltiplos para {ticker}: {e}")
+        return multiplos_resultados
 
     def analisar(self):
         if self.dados is None:
             self.baixar_dados()
 
-        # Análise de indicadores
-        for ticker in self.tickers:
-            dados_ticker = self.dados['Adj Close'][ticker]
-            print(f"Média Móvel de {ticker}: ", ind.calcular_media_movel(dados_ticker))
-            # Incluir mais chamadas de cálculos de indicadores conforme necessário
+        resultados = {
+            'indicadores': {},
+            'multiplos': self.calcular_multiplos(),
+            'portfólio': None
+        }
 
-        # Cálculo de múltiplos
-        # Isso exigiria dados adicionais que você precisaria fornecer ou obter de outra fonte
+        # Análise de indicadores
+        if 'Adj Close' in self.dados.columns:
+            for ticker in self.tickers:
+                dados_ticker = self.dados['Adj Close'][ticker]
+                resultados['indicadores'][ticker] = {
+                    'media_movel': ind.calcular_media_movel(dados_ticker),
+                    # Outros indicadores podem ser adicionados aqui
+                }
 
         # Otimização de portfólio
-        pesos, ef = mk.otimizar_portfolio(self.tickers)
-        return pesos, ef
+        try:
+            pesos, ef = mk.otimizar_portfolio(self.tickers)
+            resultados['portfólio'] = {
+                'pesos': pesos,
+                'eficiencia': ef
+            }
+        except Exception as e:
+            logging.error(f"Erro durante a otimização do portfólio: {e}")
+            raise
+
+        return resultados
 
 # Uso da classe
 def main():
     tickers = ['AAPL', 'MSFT', 'GOOG']
-    analisador = AnalisadorDeAcoes(tickers)
-    pesos, eficiencia = analisador.analisar()
+    try:
+        analisador = Acoes(tickers)
+        resultados = analisador.analisar()
+        logging.info(resultados)
+    except Exception as e:
+        logging.error(f"Erro na análise das ações: {e}")
 
 if __name__ == "__main__":
     main()
--- modules/multiplos.py
+++ modules/multiplos.py
@@ -1,7 +1,25 @@
-# Este módulo irá calcular diferentes múltiplos financeiros.
+def calcular_pl(preco, lucro_por_acao):
+    """
+    Calcula o múltiplo Preço/Lucro (P/L) de uma ação.
 
-def calcular_pe(preco, lucro_por_acao):
+    :param preco: O preço atual da ação.
+    :param lucro_por_acao: O lucro por ação (EPS - Earnings Per Share) da empresa.
+    :return: O múltiplo P/L da ação.
+    :raises ValueError: Se o lucro por ação for menor ou igual a zero.
+    """
+    if lucro_por_acao <= 0:
+        raise ValueError("O lucro por ação deve ser maior que zero.")
     return preco / lucro_por_acao
 
 def calcular_pvp(preco, valor_patrimonial_por_acao):
+    """
+    Calcula o múltiplo Preço/Valor Patrimonial (P/VP) de uma ação.
+
+    :param preco: O preço atual da ação.
+    :param valor_patrimonial_por_acao: O valor patrimonial por ação da empresa.
+    :return: O múltiplo P/VP da ação.
+    :raises ValueError: Se o valor patrimonial por ação for menor ou igual a zero.
+    """
+    if valor_patrimonial_por_acao <= 0:
+        raise ValueError("O valor patrimonial por ação deve ser maior que zero.")
     return preco / valor_patrimonial_por_acao
